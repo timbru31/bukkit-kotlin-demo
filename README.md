129f233e85ae4d100b82fa82905aaeb379d964bb
Tim Brust <ratchet.player@gmx.de>
2017-11-03 21:11:59 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# KotlinDemo
+
+## Info
+This proof of concept shows that the advantages of Kotlin can be used in the Bukkit context.
+This small plugin greets the user once he joins and can send a greeting message to all users with `/kotlin`.
+
+There are some different Kotlin syntax features that are used in the showcase.
+
+## Command
+
+| Command | Description      |
+|:--------|:-----------------|
+| kotlin  | Greets all users |
+
+---
+Built by (c) Tim Brust and contributors. Released under the MIT license.
---
+++ src/main/kotlin/de/dustplanet/kotlindemo/KotlinDemo.kt
@@ -0,0 +1,34 @@
+package de.dustplanet.kotlindemo
+
+import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.ChatColor
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.command.CommandSender
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+
+class KotlinDemo: JavaPlugin, Listener, CommandExecutor {
+    constructor(): super()
+
+    override fun onEnable() {
+        server.pluginManager.registerEvents(this, this)
+        getCommand("kotlin").setExecutor(this)
+    }
+    
+
+    @EventHandler fun onPlayerJoin(e: PlayerJoinEvent) {
+        greetPlayer(e.player, ChatColor.RED)
+    }
+
+    fun greetPlayer(player: Player, color: ChatColor) {
+        player.sendMessage("${color}You have been greeted by Kotlin in ${color.name}!")
+    }
+
+    override fun onCommand(sender: CommandSender, command: Command, commandLabel: String, args: Array<String>): Boolean {
+        server.onlinePlayers.forEach { greetPlayer(it, ChatColor.GREEN) }
+        return true
+    }
+}
